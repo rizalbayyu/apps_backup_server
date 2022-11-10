3420e99ca150694069496e14195413702164b7b6
Rizal Bayu Aji Pradana <38995446+rizalbayyu@users.noreply.github.com>
2022-11-10 16:14:09 +0700

    Update script_createUser.sh

--- script/script_createUser.sh
+++ script/script_createUser.sh
@@ -12,12 +12,14 @@ else
     mysql -e "SET GLOBAL validate_password.policy = 0;"
     echo "Start create user"
     mysql -e "CREATE USER '${USER}'@'localhost' IDENTIFIED BY '${PASSWORD}';"
+    mysql -e "CREATE USER '${USER}'@'%' IDENTIFIED BY '${PASSWORD}';"
     sleep 3s
     echo "Done"
     #Jika ingin membuat database dengan user yang telah dibuat, bisa dilakukan dengan cara berikut
     echo "Start create DB"
     mysql -e "CREATE DATABASE ${DB}"
     mysql -e "GRANT ALL PRIVILEGES ON ${DB}.* TO '${USER}'@'localhost';" #Grant privileges on specific database
+    mysql -e "GRANT ALL PRIVILEGES ON ${DB}.* TO '${USER}'@'%';" #Grant privileges on specific database
     mysql -e "FLUSH PRIVILEGES;"
     sleep 3s
     mysql -D ${DB} -e "CREATE TABLE nama_repo (repo_id VARCHAR(128) NOT NULL UNIQUE, date VARCHAR(128) NOT NULL, time VARCHAR(128) NOT NULL, last VARCHAR(128));"
