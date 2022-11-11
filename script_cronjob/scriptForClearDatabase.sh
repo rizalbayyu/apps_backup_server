5947127bb11f845bf485801867a92815911f9bde
Rizal Bayu Aji Pradana <38995446+rizalbayyu@users.noreply.github.com>
2022-11-11 15:47:04 +0700

    Update scriptForClearDatabase.sh

--- script_cronjob/scriptForClearDatabase.sh
+++ script_cronjob/scriptForClearDatabase.sh
@@ -5,6 +5,4 @@ password="devops!@#"
 
 rm /$path/$to/$backup_directory_repo/DBNamaRepo_$olddate.sql
 
-sudo ssh devops@192.168.0.140 "rm /$path/$to/$backup_directory/DBNamaRepo_$olddate.sql"
-
-echo "delete from nama_repo where date='$olddate';" | mysql -u devops -p$password -Drepo 
+#sudo ssh devops@192.168.0.140 "rm /$path/$to/$backup_directory/DBNamaRepo_$olddate.sql"
