bc84e875927011a161ba4b3d6370563e1fe39ddc
Rizal Bayu Aji Pradana <38995446+rizalbayyu@users.noreply.github.com>
2022-11-30 10:23:08 +0700

    Update scriptForClearBackup3DaysAgo.sh

--- script_cronjob/scriptForClearBackup3DaysAgo.sh
+++ script_cronjob/scriptForClearBackup3DaysAgo.sh
@@ -2,5 +2,3 @@
 oldDate=$(date --date="3 days ago" +%Y-%m-%d)
 
 sudo rm /$path/$to/$repo_archieve/backupRepo_$oldDate.tar.gz
-
-sudo mv /$path/$to/$repo_archieve/backupRepo_*.tar.gz /mnt/$external_Storage
